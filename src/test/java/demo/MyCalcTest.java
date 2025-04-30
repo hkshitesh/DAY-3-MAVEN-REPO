e9683efebd58da3f1fc14f49b27c7fc2b0da1427
Administrator <Administrator@172.31.0.188>
2025-04-30 13:49:54 +0000

    Junit code

---
+++ src/test/java/demo/MyCalcTest.java
@@ -0,0 +1,22 @@
+package demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyCalcTest {
+
+	@Test
+	public void testSum()
+	{
+		MyCalc calc = new MyCalc();
+		assertEquals(30,calc.sum(20,10));
+	}
+	@Test
+	public void testDiff()
+	{
+		MyCalc calc = new MyCalc();
+		assertEquals(10,calc.diff(20,10));
+	}
+	
+}
